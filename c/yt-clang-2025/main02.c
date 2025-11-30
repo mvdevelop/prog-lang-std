acdf0dddc6d158f2a719b0c5ff04cc0357eff0fa
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-11-29 23:16:07 -0300

    feat: adding lesson

--- c/yt-clang-2025/main02.c
+++ c/yt-clang-2025/main02.c
@@ -1,2 +1,13 @@
 
+#include <stdio.h>
 
+int main() {
+    // int age;
+    int age = 28;
+    char name[] = "Drak";
+
+    printf("My name is %s.\n", name);
+    printf("I am %d years old.\n", age);
+
+    return 0;
+}
