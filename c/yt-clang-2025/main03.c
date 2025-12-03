90b9f261c410d0bdf69c9b2be4afba7e4b2d0217
mvdevelop <marcosvmdilly@gmail.com>
2025-12-02 23:36:03 -0300

    feat: adding c data

---
+++ c/yt-clang-2025/main03.c
@@ -0,0 +1,13 @@
+
+# include <stdio>
+
+// int (4 bytes) | %d for printing
+// double (8 bytes) | %lf for printing
+// float (4 bytes) | %f for printing
+// char (1 byte) | %c for printing
+
+int main (
+
+  return 0;
+);
+
