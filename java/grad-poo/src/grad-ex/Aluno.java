a8078106965b702502dbcbc626718cb24deb7c8b
mvdevelop <marcosvmdilly@gmail.com>
2026-03-24 08:27:37 -0300

    feat: adding grad-ex java

---
+++ java/grad-poo/src/grad-ex/Aluno.java
@@ -0,0 +1,22 @@
+// package
+
+class Aluno {
+    
+    //Atributos
+    private String nome;
+    private int idade;
+    //Métodos
+    public Aluno ( String nome , int idade ) {
+         this.nome = nome;
+         this.idade = idade;
+    }
+    public void definirNome ( String nome ) {
+         this.nome = nome;
+    }  
+    public void definirIdade ( int idade ) {
+         this.idade = idade;
+    }
+    public String recuperarNome () {
+         return this.nome;
+    }     
+}
