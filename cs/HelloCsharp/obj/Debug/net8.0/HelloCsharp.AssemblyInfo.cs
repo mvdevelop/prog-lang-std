3f8120d7b8b7ffedc44cfe2f40b5eedf4913e199
mvdevelop <marcosvmdilly@gmail.com>
2026-04-15 22:11:38 -0300

    docs: adding go lang readme

--- cs/HelloCsharp/obj/Debug/net8.0/HelloCsharp.AssemblyInfo.cs
+++ cs/HelloCsharp/obj/Debug/net8.0/HelloCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9152984c45e62e013cdb74d33cef3023739d4ba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0befe16d8c5cbc607e7cf2a71458755388f7de1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
